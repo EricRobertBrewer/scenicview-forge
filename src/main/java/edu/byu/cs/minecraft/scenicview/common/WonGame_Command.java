96889593af8e004b0055cc2c34c09a9a353c95c0
Ganondox <roboiguana@gmail.com>
2019-03-18 13:39:29 -0700

    Stubbing out WonGame so escape room stops being broken

--- src/main/java/edu/byu/cs/minecraft/scenicview/common/WonGame_Command.java
+++ src/main/java/edu/byu/cs/minecraft/scenicview/common/WonGame_Command.java
@@ -28,6 +28,7 @@ class WonGame_Command  extends  CommandBase{
 
     @Override
     public void execute(@Nonnull MinecraftServer server, @Nonnull ICommandSender sender, @Nonnull String[] args) {
+        return;
     }
 
 }
